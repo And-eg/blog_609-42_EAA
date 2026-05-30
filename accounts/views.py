a4fd5dad0bb3cae2dfcd12782ca66bcce327b108
And-eg <Lex_foreve@rambler.ru>
2026-05-30 21:50:34 +0500

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return HttpResponseRedirect('/articles')
+    else:
+        form = AuthenticationForm()
+    return render(request,
+                  template_name='accounts/login.html',
+                  context={'form': form})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return HttpResponseRedirect('/articles')
---
+++ articles/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Article
+
+def article_list(request):
+    articles = Article.objects.all().order_by('date')
+    return render(request, 'articles/article_list.html', {'articles': articles})
+
+def article_item(request, slug):
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_item.html', {'article': article})
+
+def about(request):
+    return render(request, 'articles/about.html')
+
+def homepage(request):
+    return render(request, 'articles/homepage.html')
